fe18491f36cd8182352b6b960ffc1c0015ec98bc
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-09-29 12:54:57 -0700

    Add bands table

---
+++ db/migrate/20180929194604_band.rb
@@ -0,0 +1,10 @@
+class Band < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bands do |t|
+      t.string :name, null: false
+      t.text :description
+    end
+
+    add_index :bands, :name, unique: true
+  end
+end
